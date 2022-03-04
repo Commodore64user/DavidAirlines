5eba0cd5968c0d78e90f2300102d7447ebfdd080
Commodore64user <daveabc02@gmail.com>
2022-03-04 14:12:28 +0000

    initial commit

---
+++ src/main/java/com/qa/DavidAirlinesApplication.java
@@ -0,0 +1,13 @@
+package com.qa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DavidAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DavidAirlinesApplication.class, args);
+	}
+
+}
